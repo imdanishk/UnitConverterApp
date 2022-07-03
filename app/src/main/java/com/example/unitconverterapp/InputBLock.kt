68ebd3f6c00157157ac1b3259dcf727dd1d2eeba
Danish Khan <iamdanishk555@gmail.com>
2022-07-03 22:15:39 +0545

    input validation and button to convert set up

--- app/src/main/java/com/example/unitconverterapp/InputBLock.kt
+++ app/src/main/java/com/example/unitconverterapp/InputBLock.kt
@@ -1,19 +1,17 @@
 package com.example.unitconverterapp
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import android.content.Context
+import android.widget.Toast
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
-import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
@@ -23,7 +21,9 @@ import androidx.compose.ui.unit.sp
 fun InputBlock(
     conversion: Conversion,
     inputText: MutableState<String>,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    context: Context = LocalContext.current,
+    calculate: (String) -> Unit
 ) {
     Column(modifier = modifier.padding(0.dp, 20.dp, 0.dp, 0.dp)) {
         Row(modifier = modifier.fillMaxWidth()) {
@@ -50,8 +50,30 @@ fun InputBlock(
                     .padding(10.dp, 30.dp, 0.dp, 0.dp)
                     .fillMaxWidth(0.35F)
             )
+        }
+
+        Spacer(modifier = modifier.height(20.dp))
+
+        OutlinedButton(
+            onClick = {
+                if (inputText.value != "") {
+                    calculate(inputText.value)
+                } else {
+                    Toast.makeText(context, "Please, enter your value", Toast.LENGTH_LONG).show()
+                }
+            },
+            modifier = modifier.fillMaxWidth(1f)
+        )
+        {
+            Text(
+                text = "Convert",
+                fontSize = 36.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.Blue
+            )
 
         }
+
     }
 
 }
--- app/src/main/java/com/example/unitconverterapp/TopScreen.kt
+++ app/src/main/java/com/example/unitconverterapp/TopScreen.kt
@@ -1,5 +1,6 @@
 package com.example.unitconverterapp
 
+import android.util.Log
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
@@ -16,7 +17,9 @@ fun TopScreen(list: List<Conversion>) {
     }
 
     selectedConversion.value?.let {
-        InputBlock(conversion = it, inputText = inputText)
+        InputBlock(conversion = it, inputText = inputText) { input ->
+            Log.i("mytag", "User typed: $input")
+        }
     }
 }
 
